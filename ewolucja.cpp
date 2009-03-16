d229bc6394f59d03c1a99674e1bac1712f7d1e0c
Tom <tiom@niebosa.(none)>
2009-03-16 01:54:34 +0100

    Poczatek projektu

---
+++ ewolucja.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : ewolucja.cpp
+// Author      : Grupa TMG
+// Version     :
+// Copyright   : GPL
+// Description : Hello World in C++, Ansi-style
+//ssh key: naszprojekt
+//AAAAB3NzaC1yc2EAAAABIwAAAQEAuHOz+G3By8y0oC6HLI7T4IVfta2r+Xq5RnG0iYVOZ3xX6G53anHCa+GvlD7GNNuczQjiallNdPcGu0QnO82OoEilphZ0ALR5WZg0ED+LWJp15X9Pos/98vt4r8HsqQSzIQRxUx2wwPDtKwOR6CqhYqLVdbaygY5psF6YYJqMzBHzs5YKmtssivArBR0xCiAM67Px6lRZoZff8cWRY9YPo+SeEa9LhOhBpgoK1E9wfdggtDTe2D7VXjbwA/lvFEE6EnUfPFuCblbC0LTFW814W3oNn8ScxfosYZQNweY5sbp1aCbs5+d4BIKaTKkTV9kx6LCgdSgQPgp2HP1rw95Sjw==
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
